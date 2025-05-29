e695fbca7f3c3f2de2a3e4c3867e27b8c0eeccd1
nirav panesara <niravpanesaranirav@gmail.com>
2025-05-29 16:48:26 +0530

    update

--- PLANTO/client/src/Components/ProductDetails.jsx
+++ PLANTO/client/src/Components/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const ProductDetails = () => {
   return (
@@ -11,9 +11,7 @@ const ProductDetails = () => {
             src="https://dummyimage.com/400x400"
           />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              BRAND NAME
-            </h2>
+            <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
               The Catcher in the Rye
             </h1>
@@ -47,7 +45,7 @@ const ProductDetails = () => {
                 <span className="text-gray-600 ml-3">4 Reviews</span>
               </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2">
-                <a className="text-gray-500" href="#">
+                <a className="text-gray-500">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -59,7 +57,7 @@ const ProductDetails = () => {
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
                   </svg>
                 </a>
-                <a className="text-gray-500" href="#">
+                <a className="text-gray-500">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -68,10 +66,10 @@ const ProductDetails = () => {
                     className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53A4.48 4.48 0 0012 8v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
                   </svg>
                 </a>
-                <a className="text-gray-500" href="#">
+                <a className="text-gray-500">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -80,25 +78,22 @@ const ProductDetails = () => {
                     className="w-5 h-5"
                     viewBox="0 0 24 24"
                   >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8A8.5 8.5 0 018.7 3.9 8.38 8.38 0 0112.5 3h.5a8.48 8.48 0 018 8v.5z" />
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
                   </svg>
                 </a>
               </span>
             </div>
             <p className="leading-relaxed">
-              Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-              sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-              juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-              seitan poutine tumeric. Gastropub blue bottle austin listicle
-              pour-over, neutra jean shorts keytar banjo tattooed umami
-              cardigan.
+              Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia
+              microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn.
+              Everyday carry +1 seitan poutine tumeric.
             </p>
             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
               <div className="flex">
                 <span className="mr-3">Color</span>
-                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none" />
-                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none" />
-                <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none" />
+                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
               </div>
               <div className="flex ml-6 items-center">
                 <span className="mr-3">Size</span>
@@ -126,9 +121,7 @@ const ProductDetails = () => {
               </div>
             </div>
             <div className="flex">
-              <span className="title-font font-medium text-2xl text-gray-900">
-                $58.00
-              </span>
+              <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
               <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                 Button
               </button>
@@ -141,7 +134,7 @@ const ProductDetails = () => {
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
                 >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78L12 21.23l7.78-7.78a5.5 5.5 0 000-7.78z" />
+                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
                 </svg>
               </button>
             </div>
